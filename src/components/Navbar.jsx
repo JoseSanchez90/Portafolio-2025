3394b791bbb56adeb55f494c8c2aaabe0b184747
Jose Sanchez <josesancheztrujillo90@hotmail.com>
2025-01-01 22:11:12 -0500

    create new portafolio

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,53 @@
+import { useEffect } from 'react'
+import Logo from '../assets/logo.png'
+import '../css/Navbar.css'
+import { useState } from 'react';
+
+function Navbar() {
+
+  const [theme, setTheme] = useState(() => {
+    if(window.matchMedia("(prefers-color-scheme: dark)").matches){
+        return "dark";
+    }
+    return "light";
+  })
+
+      // Modo dia y noche
+      useEffect(()=> {
+        if (theme === "dark") {
+            document.querySelector('html').classList.add('dark')
+        } else {
+            document.querySelector('html').classList.remove('dark')
+        }
+    }, [theme])
+
+    const handleChangeTheme = () => {
+        setTheme(prevTheme => prevTheme === "light" ? "dark" : "light")
+    }
+    
+  return (
+
+    <header className="w-full h-screen bg-slate-100 dark:bg-gray-950">
+        <nav className="w-full flex flex-row justify-between items-center px-40 py-5">
+            <img src={Logo} alt={Logo} className="w-12" />
+            <div className="flex items-center gap-12 text-base font-semibold">
+                <li className="list-none hover:animate-jump dark:text-white text-gray-950"><a className="hover:text-green-500 " href="#">Inicio</a></li>
+                <li className="list-none hover:animate-jump dark:text-white text-gray-950"><a className="hover:text-green-500 " href="#">Habilidades</a></li>
+                <li className="list-none hover:animate-jump dark:text-white text-gray-950"><a className="hover:text-green-500 " href="#">Proyectos</a></li>
+                <li className="list-none hover:animate-jump dark:text-white text-gray-950"><a className="hover:text-green-500 " href="#">Contacto</a></li>
+
+                <label className="ui-switch">
+                  <input type="checkbox" />
+                  <div className="slider" onClick={handleChangeTheme}>
+                    <div className="circle"></div>
+                  </div>
+                </label>
+
+            </div>
+        </nav>
+    </header>
+
+  )
+}
+
+export default Navbar
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+import tailwindcssAnimated from "tailwindcss-animated";
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    tailwindcssAnimated,
+  ],
+}
