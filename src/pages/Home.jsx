698ab3db6f65c3876dcba17f35373759c648f234
Jose Sanchez <josesancheztrujillo90@hotmail.com>
2025-01-04 08:29:50 -0500

    add animated web

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -30,7 +30,7 @@ function Home() {
       {/* PERFIL  */}
       
       <div className="flex flex-col md:grid md:grid-cols-2 justify-items-center md:justify-items-end pt-20 md:pt-16 2xl:pt-24 md:px-60 px-8 py-14 gap-5 md:gap-0 items-center">
-        <div className="flex flex-col gap-2 md:gap-4 text-center md:text-start">
+        <div className="flex flex-col gap-2 md:gap-4 text-center md:text-start animate-flip-up animate-duration-[2000ms] animate-delay-500">
             <h1 className="text-xl md:text-2xl font-extrabold text-emerald-600">JOSE SANCHEZ TRUJILLO</h1>
           <div className="animation">
             <h2 className="first text-lg md:text-xl font-bold text-white"><h2>Help Desk Analyst</h2></h2>
@@ -39,10 +39,10 @@ function Home() {
           </div>
             <p className="text-sm md:text-base font-normal md:font-medium text-zinc-600 dark:text-zinc-400">Mi experiencia se basa en la práctica constante y la realización de proyectos utilizando tecnologías como HTML5, CSS3, JavaScript y ReactJS, librerías asi como Bootstrap y Tailwind. Creo experiencias de calidad y estoy siempre en la búsqueda de nuevos desafíos para aprender y crecer profesionalmente.</p>
         </div>
-        <div>
+        <div className="animate-flip-down animate-duration-[2000ms] animate-delay-500">
           <img className=" rounded-3xl size-40 sm:size-64 lg:size-60 2xl:size-72 flex-shrink-0 object-cover shadow-[10px_10px_rgba(0,_98,_90,_0.8),_15px_15px_rgba(0,_98,_90,_0.6),_20px_20px_rgba(0,_98,_90,_0.4),_25px_25px_rgba(0,_98,_90,_0.2),_30px_30px_rgba(0,_98,_90,_0.1)]" src={Profile} alt="Foto de perfil" />
         </div>
-        <div className="w-full flex flex-row justify-center md:justify-start pt-8 md:pt-6 2xl:pt-0 gap-4 md:gap-12">
+        <div className="w-full flex flex-row justify-center md:justify-start pt-8 md:pt-6 2xl:pt-0 gap-4 md:gap-12 animate-fade animate-duration-[2000ms] animate-delay-500">
           <a href="https://api.whatsapp.com/send?phone=51960041583" target="_blank">
             <button type="submit" className="flex justify-center gap-2 items-center shadow-xl text-xs bg-gray-800 dark:bg-slate-200 backdrop-blur-md isolation-auto before:absolute before:w-full before:transition-all before:duration-700 before:hover:w-full before:-left-full before:hover:left-0 before:rounded-full before:bg-green-600 text-gray-50 dark:text-gray-950 before:-z-10 before:aspect-square before:hover:scale-150 before:hover:duration-700 relative px-3 py-2 md:px-6 md:py-2 overflow-hidden rounded-full group">Conversemos
             <img src={whatsapp} alt="whatsapp" className="w-6" />
@@ -73,49 +73,49 @@ function Home() {
           <div className="images-shadow grid grid-cols-4 md:flex md:flex-row md:gap-14 gap-6 px-5">
             <div className="flex flex-col items-center gap-2">
               <a href="https://developer.mozilla.org/es/docs/Glossary/HTML5" target="_autoblank" className="text-center flex flex-col items-center gap-2">
-                <img src={html} alt={html} className="w-9 md:w-16 hover:scale-125 duration-200" />
+                <img src={html} alt={html} className="w-9 md:w-16 hover:scale-125 duration-100" />
                 <p className="text-xs md:text-sm font-medium text-black dark:text-white">Html</p>
               </a>
             </div>
             <div className="flex flex-col items-center gap-2">
               <a href="https://developer.mozilla.org/es/docs/Web/CSS" target="_autoblank" className="text-center flex flex-col items-center gap-2">
-                <img src={css} alt={css} className="w-9 md:w-16 hover:scale-125 duration-200" />
+                <img src={css} alt={css} className="w-9 md:w-16 hover:scale-125 duration-100" />
                 <p className="text-xs md:text-sm font-medium text-black dark:text-white">Css</p>
               </a>
             </div>
             <div className="flex flex-col items-center gap-2">
               <a href="https://developer.mozilla.org/es/docs/Web/JavaScript" target="_autoblank" className="text-center flex flex-col items-center gap-2">
-                <img src={js} alt={js} className="w-9 md:w-16 hover:scale-125 duration-200" />
+                <img src={js} alt={js} className="w-9 md:w-16 hover:scale-125 duration-100" />
                 <p className="text-xs md:text-sm font-medium text-black dark:text-white">JavaScript</p>
               </a>
             </div>
             <div className="flex flex-col items-center gap-2">
               <a href="https://tailwindcss.com/" target="_autoblank" className="text-center flex flex-col items-center gap-2">
-                <img src={tailwind} alt={tailwind} className="w-9 md:w-16 hover:scale-125 duration-200" />
+                <img src={tailwind} alt={tailwind} className="w-9 md:w-16 hover:scale-125 duration-100" />
                 <p className="text-xs md:text-sm font-medium text-black dark:text-white">Tailwind</p>
               </a>
             </div>
             <div className="flex flex-col items-center gap-2">
               <a href="https://getbootstrap.com/" target="_autoblank" className="text-center flex flex-col items-center gap-2">
-                <img src={bootstrap} alt={bootstrap} className="w-9 md:w-16 hover:scale-125 duration-200" />
+                <img src={bootstrap} alt={bootstrap} className="w-9 md:w-16 hover:scale-125 duration-100" />
                 <p className="text-xs md:text-sm font-medium text-black dark:text-white">BootStrap</p>
               </a>
             </div>
             <div className="flex flex-col items-center gap-2">
               <a href="https://react.dev/" target="_autoblank" className="text-center flex flex-col items-center gap-2">
-                <img src={react} alt={react} className="w-9 md:w-16 hover:scale-125 duration-200" />
+                <img src={react} alt={react} className="w-9 md:w-16 hover:scale-125 duration-100" />
                 <p className="text-xs md:text-sm font-medium text-black dark:text-white">ReactJS</p>
               </a>
             </div>
             <div className="flex flex-col items-center gap-2">
               <a href="https://github.com/" target="_autoblank" className="text-center flex flex-col items-center gap-2">
-                <img src={github} alt={github} className="w-9 md:w-16 hover:scale-125 duration-200" />
+                <img src={github} alt={github} className="w-9 md:w-16 hover:scale-125 duration-100" />
                 <p className="text-xs md:text-sm font-medium text-black dark:text-white">GitHub</p>
               </a>
             </div>
             <div className="flex flex-col items-center gap-2">
               <a href="https://firebase.google.com/" target="_autoblank" className="text-center flex flex-col items-center gap-2">
-                <img src={firebase} alt={firebase} className="w-9 md:w-16 hover:scale-125 duration-200" />
+                <img src={firebase} alt={firebase} className="w-9 md:w-16 hover:scale-125 duration-100" />
                 <p className="text-xs md:text-sm font-medium text-black dark:text-white">Firebase</p>
               </a>
             </div>        
@@ -145,25 +145,25 @@ function Home() {
           <div className="images-shadow grid grid-cols-2 md:flex md:flex-row md:gap-14 gap-10">
             <div className="flex flex-col items-center gap-2">
               <a href="https://api.whatsapp.com/send?phone=51960041583" target="_blank" className="text-center flex flex-col items-center gap-2">
-                <img src={whatsapp} alt="" className="w-9 md:w-16 hover:scale-125 duration-200" />
+                <img src={whatsapp} alt="" className="w-9 md:w-16 hover:scale-125 duration-100" />
                 <p className="text-sm font-medium text-black dark:text-white">Whatsapp</p>
               </a>
             </div>
             <div className="flex flex-col items-center gap-2">
               <a href="https://t.me/josesanchezt" target="_autoblank" className="text-center flex flex-col items-center gap-2">
-                <img src={telegram} alt="" className="w-9 md:w-16 hover:scale-125 duration-200" />
+                <img src={telegram} alt="" className="w-9 md:w-16 hover:scale-125 duration-100" />
                 <p className="text-sm font-medium text-black dark:text-white">Telegram</p>
               </a>
             </div>
             <div className="flex flex-col items-center gap-2">
               <a href="mailto:josesancheztrujillo7@gmail.com" target="_autoblank" className="text-center flex flex-col items-center gap-2">
-                <img src={gmail} alt="" className="w-9 md:w-16 hover:scale-125 duration-200" />
+                <img src={gmail} alt="" className="w-9 md:w-16 hover:scale-125 duration-100" />
                 <p className="text-sm font-medium text-black dark:text-white">Gmail</p>
               </a>
             </div>
             <div className="flex flex-col items-center gap-2">
               <a href="https://www.linkedin.com/in/josesanchez90/" target="_autoblank" className="text-center flex flex-col items-center gap-2">
-                <img src={linkedin} alt="" className="w-9 md:w-16 hover:scale-125 duration-200" />
+                <img src={linkedin} alt="" className="w-9 md:w-16 hover:scale-125 duration-100" />
                 <p className="text-sm font-medium text-black dark:text-white">LinkedIn</p>
               </a>
             </div>
